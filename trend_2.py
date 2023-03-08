914df75deec23af8dc8e86b0837dcfaeb85f5399
kwieczorek <wieczorek.konrad@gmail.com>
2023-03-08 17:34:19 +0200

    1

---
+++ trend_2.py
@@ -0,0 +1,41 @@
+"""
+(main page)
+Today, I want to share with you a research we conducted on the price trends of our top four major instruments -
+EURUSD, GBPUSD, XAUUSD, and USDJPY, over the period from April 2015 to February 2023. Our objective was to understand
+how our clients perform in trending markets, versus ranging markets, and how this affects our profitability.
+
+(charts)
+To begin with, we focused on short-term trends, highlighted here in green, identified using the Choppiness Index, which
+is a volatility indicator that helps determine if the market was trading sideways or within a trend on historical data.
+
+(main - summary)
+We found that our top four instruments were trending on average 33% of the time, and that clients realized approximately
+68% of our profits during these trending periods. The average duration of a trending phase was 22 days.
+
+(seasonality - charts)
+We also examined seasonality by analyzing the number of trending days in each month, and the average realized PnL during
+these periods, excluding the day that the war started. We found that March, April, July, September,
+and November were generally good months for us, while October and December tended to be challenging.
+
+(top 4 - last 3 months)
+Overall, our analysis showed that the last three months were unusual compared to historical data, with no trending
+periods for three of the four instruments.
+
+(DXY)
+The reason for this could be the price action of the US dollar, which is either quoted or the base currency for all four
+symbols. In the last three months, the US dollar has remained relatively stable, with no major short-term moves in any
+direction. As a result, our realized PnL during the trending phase was just 16M compared to the average of 40M, and the
+overall profit for this timeframe on top4 instruments was 31M compared to 60M on average.
+
+In summary, our research indicates that our clients perform bad during trending periods and volatile sessions, as
+discussed last week, and that our profitability is closely linked to the frequency and duration of these trends and
+moves.
+
+(realized - summary)
+While the last three months have been atypical historically speaking, we anticipate that the period from March
+to September will be more favorable in terms of profitability. We believe that seasonality and the reversion to the mean
+for the number of trending and volatile days will be a positive factor going forward. Based on historical data for this
+period we can estimate an average realized gain of 25M monthly. Thank you.
+
+
+"""
