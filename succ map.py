495edc9467bdee235359605ba6376d006167eb5c
formaaaa <wieczorek.konrad@gmail.com>
2024-05-22 15:53:41 +0300

    1

---
+++ succ map.py
@@ -0,0 +1,64 @@
+"""
+Q3 Overview
+Expected Result:
+
+    Develop an overall vision of the team processes.
+    Understand how different product features relate and affect each other. (how does amending of trading conditions
+    impact the volume and profitability, how to use different tools and dashboards that we have)
+    Participate in at least three R&A tasks/projects with team mentorship.
+
+Key Metrics (KR):
+
+    KR1: Completion of assigned tasks/projects (Yes/No).
+    KR2: Ability to delegate routine processes to the newcomer.
+
+Actions:
+
+    Discoveries/Project: Engage in discovery projects.
+    SQL Work: Utilize SQL to complete tasks.
+    Data Analysis: Work with data to draw conclusions.
+    Feedback/Correction Meetings: Participate in feedback sessions if needed.
+
+Q4 Overview
+Expected Result:
+
+    Propose and justify potential trading condition changes using R&A tools.
+    Calculate and offer suggestions for Octa Promos (spread, leverage).
+    Explain profit considerations (symbols/assets classes, seasonality).
+
+Key Metrics (KR):
+
+    KR1: Verification and approval of trading conditions change proposals by R&A at least once.
+    KR2: Verification and approval of promo proposals by R&A at least once.
+
+Actions:
+
+    R&A Reports: Learn to read R&A reports and draw conclusions.
+    Promo Calculator: Use the promo calculator for dynamic analysis of symbols/assets classes.
+    Feedback/Correction Meetings: Attend feedback sessions if necessary.
+
+Q1 Overview
+Expected Result:
+
+    Understand the relation and cross-impact of company main business metrics. (volume, overall profitability,
+    profit from spreads and from market movement, VpT, PF, the behaviour of different cohorts of clients)
+    Keep up with competitors by understanding our conditions vs. theirs. (new products, bonuses, promotions,
+    partnership programs)
+    Complete complex projects with minimal supervision (e.g., adding new symbols).
+
+Key Metrics (KR):
+
+    KR: Completion of a complex task/project.
+
+Actions:
+
+    Business Metrics Analysis: Perform basic analysis of the company by business metrics.
+    Hypothesis Definition: Define and test your own hypotheses.
+    Feedback/Correction Meetings: Engage in feedback meetings if required.
+
+Conclusion
+
+    This roadmap ensures a structured and progressive onboarding process.
+    Each quarter focuses on developing specific skills and knowledge.
+    Regular feedback and correction meetings help in continuous improvement
+"""
