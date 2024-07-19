be180483633ef71c7693b8b1f2207396f62ef5d1
formaaaa <wieczorek.konrad@gmail.com>
2024-07-19 18:24:49 +0300

    1

--- weekly.py
+++ weekly.py
@@ -1,13 +1,16 @@
 """
-1. Last week we were testing a trading bot to gather information on competitors' execution.
-This week we will deposit accounts, and start testing on 0.01 lot size, if all goes well we will move up to 1 lot.
+1. Last week we were building and testing our trading bot on competitors demo account.
+This week we will deposit real accounts, and already start gathering data
+we'll start from 0.01 lot size, if all goes well we will move up to bigger positions.
 
-2. We did some approximations regarding additional PnL increases we can expect from additional news delta extensions,
-on top4 majors and top4 cryptos, and it looks like we can get additional 0.5m a quarter.
+(2. We did some approximations how increasing our spreads on news just by 10% would impact our PnL,
+on top4 majors and top4 cryptos, and it looks like we could get additional 0.5m a quarter.
+We will present the final proposal for extensions, after they are done with testing, and have all the necessary data.)
 
-3. We have a new monthly task, to investigate major differences in PnL per lot for different GEOs. We did the 1st one
-manually, and now working on automatizing it.
+3. We also started working on a new task, to investigate out of the ordinary monthly differences in PnL for
+different countries.
+We did the 1st one manually, and now working on automatizing it.
 
 4. And we started research about potentially onboarding new LPs that can give us regional instruments, like Asian
-indices, Indian stocks and also new commodities.
+indices, Indian stocks and also new commodities. And will be collaborating on this with CY dealers.
 """
